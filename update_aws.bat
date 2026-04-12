cec1f6817754e83e8d080601da4bfab02d8a4792
evkisawari <137887334+evkisawari@users.noreply.github.com>
2026-04-13 01:00:54 +0530

    Auto-update from laptop

--- update_aws.bat
+++ update_aws.bat
@@ -16,11 +16,11 @@ git push origin main
 echo.
 echo [2/3] Connecting to AWS and pulling new code...
 :: This runs commands on the AWS server remotely
-ssh -o StrictHostKeyChecking=no -i "%PEM_NAME%" ec2-user@%AWS_IP% "cd %REMOTE_DIR% && git pull origin main"
+C:\Windows\System32\OpenSSH\ssh.exe -o StrictHostKeyChecking=no -i "%PEM_NAME%" ec2-user@%AWS_IP% "cd %REMOTE_DIR% && git pull origin main"
 
 echo.
 echo [3/3] Restarting the Docker container...
-ssh -o StrictHostKeyChecking=no -i "%PEM_NAME%" ec2-user@%AWS_IP% "sudo docker stop news-server news-live || true && sudo docker rm news-server news-live || true && cd %REMOTE_DIR% && sudo docker build -t news-backend . && sudo docker run -d -p 8000:8000 --name news-server news-backend"
+C:\Windows\System32\OpenSSH\ssh.exe -o StrictHostKeyChecking=no -i "%PEM_NAME%" ec2-user@%AWS_IP% "sudo docker stop news-server news-live || true && sudo docker rm news-server news-live || true && cd %REMOTE_DIR% && sudo docker build -t news-backend . && sudo docker run -d -p 8000:8000 --name news-server news-backend"
 
 echo.
 echo ==========================================
